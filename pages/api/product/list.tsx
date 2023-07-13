8d47e7a2ce181b291ed9ef8277d380ff0ccb155c
Reynald Gragasin <reynald@Reynalds-MacBook-Pro.local>
2023-07-14 02:36:59 +0800

    NextJS 13, Prisma, MongoDB-Atlas

--- pages/api/product/list.tsx
+++ pages/api/product/list.tsx
@@ -1,5 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-// import prisma from '../../../lib/prisma';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const prisma = new PrismaClient();
@@ -7,18 +6,9 @@ const prisma = new PrismaClient();
 export default async function list(req: NextApiRequest, res: NextApiResponse) {
     const { query } = req;
     try {
-
-        // let perpage: any = 5;
-        // let recs = await prisma.product.findMany();
-        // let totrecs: any = recs?.length;
-        // let totpage: any = Math.ceil(totrecs / perpage);
-        // let page: any = query?.page;
-        // let offset = (page -1) * perpage;
-
-        let perpage: any = 5;
         const response = await prisma.product.findMany();
-        let data = await response.json();
-        let totrecs: any = data.length;
+        let perpage: any = 5;
+        let totrecs: any = response.length;
         let totpage: any = Math.ceil(totrecs / perpage);
         let page: any = query?.page;
         let offset: any = (page -1) * perpage;
