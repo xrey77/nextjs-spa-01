5168ad33265fa6f0aeeedc8a218cec8cb618b110
Reynald Gragasin <reynald@Reynalds-MacBook-Pro.local>
2023-07-14 12:04:47 +0800

    NextJS 13, Prisma, MongoDB-Atlas

--- pages/aboutus.tsx
+++ pages/aboutus.tsx
@@ -3,8 +3,10 @@ import styles from '../styles/Home.module.css'
 const Aboutus = () => {
     return(
         <main className={styles.main}>
+        <div>
            <h1>About Us Page</h1>
-           <div><p className="text-center">Under Construction</p></div>
+            <p className="text-center">Under Construction</p>
+        </div>
         </main>
     )
 }
